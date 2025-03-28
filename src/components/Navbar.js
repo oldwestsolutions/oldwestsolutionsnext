3075dad7b81f77c6196048f978df40974fb6b982
Your Name <your.email@example.com>
2025-03-28 17:54:59 -0500

    Removed navigation links and image placeholder from desktop view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import { useEffect } from 'react';
-import Image from 'next/image';
 
 export default function Navbar() {
   useEffect(() => {
@@ -14,36 +13,8 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
       <div className="container">
         <Link href="/" className="navbar-brand">
-          <Image
-            src="/logo.png"
-            alt="Old West Solutions"
-            width={40}
-            height={40}
-            className="d-inline-block align-text-top me-2 d-none d-md-inline-block"
-          />
-          <Image
-            src="/logo.png"
-            alt="Old West Solutions"
-            width={32}
-            height={32}
-            className="d-inline-block align-text-top me-2 d-md-none"
-          />
-          <span className="d-none d-md-inline">Old West Solutions</span>
+          OLD WEST SOLUTIONS
         </Link>
-        <div className="navbar-nav ms-auto d-none d-md-flex">
-          <Link href="/" className="nav-link">
-            Home
-          </Link>
-          <Link href="/domains" className="nav-link">
-            Domains
-          </Link>
-          <Link href="/networks" className="nav-link">
-            Networks
-          </Link>
-          <Link href="/servers" className="nav-link">
-            Servers
-          </Link>
-        </div>
       </div>
     </nav>
   );
